b72901d72a27675414e2408bc588c2ec56373be4
sandybeyer <64211884+sandybeyer@users.noreply.github.com>
2020-11-06 13:31:28 -0500

    Upgrade to latest graphics

--- gift1/src/App.js
+++ gift1/src/App.js
@@ -1,22 +1,92 @@
 import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Fullbasket from "./components/fullbasket";
-import Home from "./components/home";
-import Navbar from "./components/Navbar";
+import "./index.css";
+import data from "./data.json";
+import Products from "./components/Products";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/fullbasket" component={Fullbasket} />
-        </Switch>
-      </BrowserRouter>
-    </div>
-  );
+import Cart from "./components/Cart";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      cartItems: [],
+      size: "",
+      sort: "",
+    };
+  }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+  };
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems });
+  };
+  sortProducts(event) {
+    const sort = event.target.value;
+    console.log(event.target.value);
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    }));
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">Gift Basket Heaven</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              ></Products>
+            </div>
+
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
+            </div>
+          </div>
+        </main>
+        <footer>Best present ever!</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
